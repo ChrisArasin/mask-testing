3db0c89f6ad796bad6577b6771190f2c358b0893
Chris Arasin <chris.arasin@gmail.com>
2019-06-06 10:58:39 -0400

    build added for gh pages

---
+++ docs/precache-manifest.6cf23752b75417d9bdb1486b04160607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7fd0d00d528fe96de332c43ac589af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7127c9e8c7ad948b600",
+    "url": "/static/css/main.0b1efe8f.chunk.css"
+  },
+  {
+    "revision": "5b3eb688ab4235402a23",
+    "url": "/static/js/2.db25e522.chunk.js"
+  },
+  {
+    "revision": "b7127c9e8c7ad948b600",
+    "url": "/static/js/main.a172ea4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99a0a237b8eae1c0e612f0db68d27af8",
+    "url": "/static/media/pupper.99a0a237.jpg"
+  }
+]);
